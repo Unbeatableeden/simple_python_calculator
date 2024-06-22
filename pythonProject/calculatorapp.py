25187a400c1f33b7590ba44248c939b5d662f98f
Unbeatableeden <Unbeatableeden7@gmail.com>
2024-06-22 20:02:45 +0100

    Update calculatorapp.py

--- pythonProject/calculatorapp.py
+++ pythonProject/calculatorapp.py
@@ -1,20 +1,31 @@
-operator = input("enter an operator (+ - * /): ")
-num1 = float(input("enter the first number: "))
-num2 = float(input("enter the second number: "))
+while True:
+    operator = input("Please enter an operator (+, -, *, /): ")
+    if operator == "+" or operator == "-" or operator == "*" or operator == "/":
+        break
+    else:
+        print("Invalid operator, please enter an operator")
+
+
+num1 = float(input("enter the first number "))
+num2 = float(input("enter the first number "))
 
 if operator == "+":
- result = num1 + num2
- print(round(result, 3))
+    result = num1 + num2
+    print(round(result, 3))
+
 elif operator == "-":
- result = num1 - num2
- print(round(result, 3))
+    result = num1 - num2
+    print(round(result, 3))
+
 elif operator == "*":
- result = num1 * num2
- print(round(result, 3))
+    result = num1 * num2
+    print(round(result, 3))
+
 elif operator == "/":
- result = num1 / num2
- print(round(result, 3))
+    result = num1 / num2
+    print(round(result, 3))
 else:
- print(f"{operator} a invalid operator ")
+    print(f"{operator} not found ! ")
 
 
+  
