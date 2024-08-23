810377396eeaf6e1c45202db00da3cd8f0e02524
Unbeatableeden <Unbeatableeden7@gmail.com>
2024-08-23 23:57:32 +0100

    Update calculatorapp.py
    The Python Calculator App is a versatile and user-friendly application designed to perform a wide range of arithmetic operations. Built using Python, this app is perfect for anyone looking to perform quick calculations or learn more about Python programming. The app features a clean and intuitive interface, making it accessible to users of all skill levels.

--- pythonProject/calculatorapp.py
+++ pythonProject/calculatorapp.py
@@ -1,31 +1,54 @@
-while True:
-    operator = input("Please enter an operator (+, -, *, /): ")
-    if operator == "+" or operator == "-" or operator == "*" or operator == "/":
-        break
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "error! division by zero is not allowed."
     else:
-        print("Invalid operator, please enter an operator")
+        return x / y
+
+print("Select operation:")
+print("1. Add")
+print("2. subtract")
+print("3. Multiply")
+print("4. divide")
+print("5. stop")
+
+while True:
+    choice = input("enter choice (1/2/3/4/5) : ")
+
+    if choice == "5":
+        break
+
+    if choice in ("1", "2", "3", "4"):
+        num1 = float(input("enter first number: "))
+        num2 = float(input("enter second number: "))
 
 
-num1 = float(input("enter the first number "))
-num2 = float(input("enter the first number "))
+        if  choice == "1":
+            print(num1, "+", num2, "=", add(num1, num2))
 
-if operator == "+":
-    result = num1 + num2
-    print(round(result, 3))
+        elif choice == "2":
+            print(num1, "-", num2, "=", subtract(num1, num2))
 
-elif operator == "-":
-    result = num1 - num2
-    print(round(result, 3))
+        elif choice == "3":
+            print(num1, "*", num2, "=", multiply(num1, num2))
 
-elif operator == "*":
-    result = num1 * num2
-    print(round(result, 3))
+        elif choice == "4":
+            print(num1, "/", num2, "=", divide(num1, num2))
 
-elif operator == "/":
-    result = num1 / num2
-    print(round(result, 3))
-else:
-    print(f"{operator} not found ! ")
+        else:
+            print("invalid input")
 
 
-  
+
+
+
+
+
